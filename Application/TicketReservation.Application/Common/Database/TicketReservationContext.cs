e23e2c65c64b476afcbd9cb75aad5cd1734fdf2e
JachuPL <bonkowskijan@gmail.com>
2019-06-28 22:00:26 +0200

    Added database integration via Entity Framework Core

--- Application/TicketReservation.Application/Common/Database/TicketReservationContext.cs
+++ Application/TicketReservation.Application/Common/Database/TicketReservationContext.cs
@@ -1,8 +1,81 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using TicketReservation.Application.Cinemas.Mappings;
+using TicketReservation.Application.Movies.Mappings;
+using TicketReservation.Application.Reservations.Mappings;
+using TicketReservation.Application.Shows.Mappings;
+using TicketReservation.Domain;
+using TicketReservation.Domain.Identity;
 
 namespace TicketReservation.Application.Common.Database
 {
-    public class TicketReservationContext : IdentityDbContext
+    public class TicketReservationContext : IdentityDbContext<User, Role, Guid, UserClaim, UserRole, UserLogin, RoleClaim, UserToken>
     {
+        public virtual DbSet<Cinema> Cinemas { get; set; }
+        public virtual DbSet<Movie> Movies { get; set; }
+        public virtual DbSet<Show> Shows { get; set; }
+
+        protected TicketReservationContext()
+        {
+        }
+
+        public TicketReservationContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseInMemoryDatabase("InMemoryDatabase");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfiguration(new CinemaMapping());
+            builder.ApplyConfiguration(new CinemaMovieMapping());
+            builder.ApplyConfiguration(new MovieMapping());
+            builder.ApplyConfiguration(new ReservationMapping());
+            builder.ApplyConfiguration(new ReservedSeatMapping());
+            builder.ApplyConfiguration(new ShowMapping());
+
+            ConfigureIdentity(builder);
+        }
+
+        private static void ConfigureIdentity(ModelBuilder builder)
+        {
+            builder.Entity<Role>().ToTable("Roles");
+            builder.Entity<RoleClaim>().ToTable("RoleClaims");
+            builder.Entity<User>().ToTable("Users");
+            builder.Entity<UserLogin>().ToTable("UserLogins");
+            builder.Entity<UserRole>().ToTable("UserRoles");
+            builder.Entity<UserToken>().ToTable("UserTokens");
+            builder.Entity<UserClaim>().ToTable("UserClaims");
+
+            Role administrator = new Role
+            {
+                Id = Guid.NewGuid(),
+                Name = "Administrator",
+                NormalizedName = "Administrator"
+            };
+
+            Role cashier = new Role
+            {
+                Id = Guid.NewGuid(),
+                Name = "Cashier",
+                NormalizedName = "Cashier"
+            };
+
+            Role customer = new Role
+            {
+                Id = Guid.NewGuid(),
+                Name = "Customer",
+                NormalizedName = "Customer"
+            };
+            builder.Entity<Role>().HasData(administrator, cashier, customer);
+        }
     }
 }
--- Application/TicketReservation.WebAPI/Startup.cs
+++ Application/TicketReservation.WebAPI/Startup.cs
@@ -1,15 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using TicketReservation.Application.Common.Database;
+using TicketReservation.Domain.Identity;
 
 namespace TicketReservation.WebAPI
 {
@@ -23,10 +24,50 @@ namespace TicketReservation.WebAPI
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services, IHostingEnvironment environment)
         {
             services.AddCors();
+            services.AddIdentity<IdentityUser, IdentityRole>().AddRoles<Role>();
+            services.AddDbContext<TicketReservationContext>(opts =>
+                {
+                    opts.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+                });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            if (environment.IsDevelopment())
+            {
+                services.AddSwaggerGen(c =>
+                {
+                    c.SwaggerDoc("v1", new Info { Title = "Ticket Reservation", Version = "v1" });
+
+                    var security = new Dictionary<string, IEnumerable<string>>
+                    {
+                        {"Bearer", new string[] { }},
+                    };
+
+                    c.AddSecurityDefinition("Bearer", new ApiKeyScheme
+                    {
+                        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                        Name = "Authorization",
+                        In = "header",
+                        Type = "apiKey"
+                    });
+                    c.AddSecurityRequirement(security);
+                });
+            }
+
+            services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(x =>
+                {
+                    x.RequireHttpsMetadata = environment.IsProduction();
+                    x.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidIssuer = "http://localhost:5000",
+                        ValidateAudience = false,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("kSJV4F2H7Sh)WS7*DC%x(#pYn#5GXy"))
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +76,11 @@ namespace TicketReservation.WebAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ticket Reservation");
+                });
             }
             else
             {
@@ -49,6 +95,8 @@ namespace TicketReservation.WebAPI
                 builder.AllowAnyMethod();
                 builder.AllowCredentials();
             });
+
+            app.UseAuthentication();
 
             app.UseHttpsRedirection();
             app.UseMvc();
